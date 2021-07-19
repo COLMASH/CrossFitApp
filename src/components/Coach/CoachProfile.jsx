3ae2e62fc1c37d9caf393432cf9aa4723c830e4c
welfar <83843290+welfar@users.noreply.github.com>
2021-07-19 07:32:43 -0500

    Update CoachProfile.jsx
    se corrige indentación y espacios vacios

--- src/components/Coach/CoachProfile.jsx
+++ src/components/Coach/CoachProfile.jsx
@@ -2,13 +2,6 @@ import { useSelector } from "react-redux";
 import UpdateCoachProfilePic from "../../components/Coach/profilePicUpdate";
 
 function CoachProfile() {
-
-  
-  
-  
-  
-  
-  
   const { coach } = useSelector((state) => {
     return {
       coach: state.selectCoachReducer.coach,
@@ -22,25 +15,25 @@ function CoachProfile() {
             <div className="d-flex flex-column align-items-center text-center">
               <img
              	src={coach.profilePicture}
-								alt="Coach"
-								className="rounded-circle"
-								width="150"
+		alt="Coach"
+		className="rounded-circle"
+		width="150"
               />
-                	<button
-								type="image"
-								src="../../assets/images/camera.png"
-								data-bs-toggle="modal"
-								data-bs-target="#staticBackdrop"
-								className="homeButton btn btn-primary btn-sm"
-							>
-								Update
-							</button>  
+	      <button
+		type="image"
+		src="../../assets/images/camera.png"
+		data-bs-toggle="modal"
+		data-bs-target="#staticBackdrop"
+		className="homeButton btn btn-primary btn-sm"
+	      >
+		Update
+	      </button>  
               <div className="mt-3">
                 <h1 style={{ fontSize: 25 }}>
                   {coach.name} {coach.lastname}
                 </h1>
                 <p>{coach.email}</p>
-								<p>{coach.phone}</p>
+		<p>{coach.phone}</p>
               </div>
             </div>
             <UpdateCoachProfilePic />
