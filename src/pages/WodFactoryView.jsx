1e3a760af4d3243e0970ed9b831cb482ae33e422
Miguel Santana H <mi_santa@hotmail.com>
2021-07-23 08:13:06 -0500

    elimina codigo comentado

--- src/pages/WodFactoryView.jsx
+++ src/pages/WodFactoryView.jsx
@@ -2,19 +2,11 @@ import CoachProfile from "../components/Coach/CoachProfile";
 import CoachBar from "../components/Coach/CoachBar";
 import WodManager from "../components/Wod/WodManager";
 import WodList from "../components/Wod/WodList";
-import { getWod, getAllWod } from "../store/selectWodReducer";
-import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
 
 import "../styles/CreateWod.css";
 import "../styles/AdminsView.css";
 
 function WodFactoryView() {
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(getWod());
-    dispatch(getAllWod());
-  }, []);
   return (
     <div className="mainCreateWod">
       <div className="coach-blockNavInfo">
