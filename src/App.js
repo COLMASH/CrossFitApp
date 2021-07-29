800655ca1b68d8c353fea7e145626e59f0adab31
Miguel Santana H <mi_santa@hotmail.com>
2021-07-29 15:04:07 -0500

    corrige ruta en desuso

--- src/App.js
+++ src/App.js
@@ -16,7 +16,6 @@ import WodFactoryView from "./pages/WodFactoryView";
 import UserSuscribeWod from "./pages/UserSuscribeWod";
 import UserWodReservations from "./pages/UserWodReservations";
 import NewsView from "./pages/NewsView";
-import coachUserView from "./pages/CoachUsersView";
 import PaymentsView from "./pages/PaymentsView";
 
 function App() {
